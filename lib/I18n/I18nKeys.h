71b5dba21edccac21127cb3085187a37f76acb84
Unintendedsideeffects <malcolmcosenza@gmail.com>
2026-05-14 15:33:47 +0200

    style: clang-format I18nKeys V1_LANGUAGES table

--- lib/I18n/I18nKeys.h
+++ lib/I18n/I18nKeys.h
@@ -538,9 +538,6 @@ static_assert(sizeof(SORTED_LANGUAGE_INDICES) / sizeof(SORTED_LANGUAGE_INDICES[0
 
 // V1 language.bin migration table (frozen enum order from 2f969a9)
 constexpr Language V1_LANGUAGES[] = {
-    Language::EN,      Language::ES, Language::FRENCH, Language::DE, Language::CZECH, Language::PORTUGUESE,
-    Language::RUSSIAN, Language::SV, Language::RO,     Language::CA, Language::UK,    Language::BE,
-    Language::IT,      Language::PL, Language::FI,     Language::DA, Language::NL,    Language::TR,
-    Language::KK,      Language::HU, Language::LT,     Language::SI,
+    Language::EN, Language::ES, Language::FRENCH, Language::DE, Language::CZECH, Language::PORTUGUESE, Language::RUSSIAN, Language::SV, Language::RO, Language::CA, Language::UK, Language::BE, Language::IT, Language::PL, Language::FI, Language::DA, Language::NL, Language::TR, Language::KK, Language::HU, Language::LT, Language::SI,
 };
 constexpr uint8_t V1_LANGUAGE_COUNT = 22;
